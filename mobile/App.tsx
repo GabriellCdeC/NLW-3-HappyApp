36e9769f03d8bbff626895d942f0efd397908512
Gabriell Castro de Carvalho <gabriellcastrocarvalho@gmail.com>
2020-10-15 16:29:13 -0300

    create app
    Create app for mobile using react native

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+import Routes from './src/routes'
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
